f4d27b8f4d08fec7e7beef08d2a8764f3099909f
dkokamoto <d.okamoto@sfu.ca>
2016-03-13 11:17:09 -0700

    Updated figures and code

--- 2_Data_Formatting_Code/Data_prep.R
+++ 2_Data_Formatting_Code/Data_prep.R
@@ -6,62 +6,80 @@ lapply(package.list,library,character.only = T)
 
 ### load urchin data ###
 settlement <- read.csv("./1_Data/Invertebrate_Settlement_All_Years.csv",header=T)
-### format dates, etc.
-settlement$DATE_RETRIEVED <- parse_date_time(settlement$DATE_RETRIEVED, "%Y-%m-%d")
-settlement$DATE_DEPLOYED<- parse_date_time(settlement$DATE_DEPLOYED, "%Y-%m-%d")
 
-settlement$DATE_RETRIEVED <- parse_date_time(settlement$DATE_RETRIEVED, "%Y-%m-%d")
-settlement$DATE_DEPLOYED<- parse_date_time(settlement$DATE_DEPLOYED, "%Y-%m-%d")
-settlement$julian <- as.numeric(julian(settlement$DATE_RETRIEVED))
-settlement$ID <- with(settlement,paste(DATE_DEPLOYED,DATE_RETRIEVED,SITE),sep= "_")
-settlement$ID2 <- with(settlement,paste(DATE_DEPLOYED,SITE),sep= "_")
-settlement$ID3 <- with(settlement,paste(DATE_RETRIEVED,SITE),sep= "_")
-settlement$month_dep <- month(settlement$DATE_DEPLOYED)
-settlement$month_ret <- month(settlement$DATE_RETRIEVED)
-settlement$year_dep <- year(settlement$DATE_DEPLOYED)
-settlement$year_ret <- year(settlement$DATE_RETRIEVED)
-settlement$day_dep <- day(settlement$DATE_DEPLOYED)
-settlement$day_ret <- day(settlement$DATE_RETRIEVED)
-settlement$S_FRANCISCANUS[settlement$year_ret==2009&settlement$month_ret==04&settlement$day_ret==07&is.na(settlement$S_FRANCISCANUS)&!is.na(settlement$S_PURPURATUS)] <- 0
-settlement <- subset(settlement,!is.na(S_FRANCISCANUS)&!is.na(S_PURPURATUS)&!is.na(TOTAL_URCHINS))
+
+### format dates, etc.
+
+settlement <-  mutate(settlement, 
+                       DATE_RETRIEVED=parse_date_time(DATE_RETRIEVED, "%Y-%m-%d"),
+                       DATE_RETRIEVED=parse_date_time(DATE_RETRIEVED, "%Y-%m-%d"))
+
+settlement <- mutate(settlement,
+                      julian= as.numeric(julian(DATE_RETRIEVED)),
+                      ID = paste(DATE_DEPLOYED,DATE_RETRIEVED,SITE,sep= "_"),
+                      ID2 =paste(DATE_DEPLOYED,SITE,sep= "_"),
+                      ID3 =paste(DATE_RETRIEVED,SITE,sep= "_"),
+                      month_dep= month(DATE_DEPLOYED),
+                      month_ret = month(DATE_RETRIEVED),
+                      year_dep = year(DATE_DEPLOYED),
+                      year_ret = year(DATE_RETRIEVED),
+                      day_dep = day(DATE_DEPLOYED),
+                      day_ret = day(DATE_RETRIEVED))
+
+settlement2 <-  settlement %>% mutate(S_FRANCISCANUS = replace(S_FRANCISCANUS, 
+      year_ret==2009&
+      month_ret==04&
+        day_ret==07&
+        is.na(S_FRANCISCANUS)&
+        !is.na(S_PURPURATUS)
+        , 0))  
+
+settlement <- settlement%>%
+              mutate(diff = as.numeric(Duration)) %>%
+              filter(!is.na(S_FRANCISCANUS)&
+                       !is.na(S_PURPURATUS)&
+                       !is.na(TOTAL_URCHINS)&
+                       Duration>0) %>%
+              arrange(SITE,julian) %>%
+              drop.levels
+
 
 ### format data to estimate collection period means per brush per day
-settlement$SITE <- factor(settlement$SITE,levels= unique(settlement$SITE))
-settlement <- settlement[order(settlement$SITE,settlement$julian),]
-settlement$diff <- as.numeric(settlement$Duration)
-settlement <- drop.levels(settlement)
-settlement<- subset(settlement,Duration>0)
-set.sum <- ddply(settlement,.(SITE,DATE_RETRIEVED,DATE_DEPLOYED,month_ret,year_ret,Duration),summarise, SP= sum(S_PURPURATUS),SF= sum(S_FRANCISCANUS),TOT= sum(TOTAL_URCHINS),NB= length(TOTAL_URCHINS))
 
+set.sum <- settlement %>% group_by(SITE,DATE_RETRIEVED,DATE_DEPLOYED,month_ret,year_ret,Duration) %>%
+  summarise(SP= sum(S_PURPURATUS),
+            SF= sum(S_FRANCISCANUS),
+            TOT= sum(TOTAL_URCHINS),
+            NB= length(TOTAL_URCHINS)) %>%
+  mutate(SP_EM= as.numeric(ifelse(SP+SF==0&TOT>0,NA, ifelse(SP+SF==0,0,SP/(SP+SF)*TOT)/Duration/NB)),
+         SF_EM = as.numeric(ifelse(SP+SF==0&TOT>0,NA, ifelse(SP+SF==0,0,SF/(SP+SF)*TOT)/Duration/NB)),
+         julian = julian(DATE_RETRIEVED, origin = "1990-01-01"),
+         SITE_NUM = as.numeric(SITE),
+         M1 = cos(2*pi*month_ret/26),
+         M2 = cos(2*pi*month_ret/26),
+         yday = yday(DATE_RETRIEVED),
+         biweek  = floor(yday/14)+1) %>%
+  mutate(biweek =ifelse(biweek>26,26,biweek),
+          biweek_year  = biweek+(year_ret-1990)*26) %>%
+  filter(!is.na(TOT)&Duration<45) 
 
-set.sum$SP_EM <- with(set.sum,ifelse(SP+SF==0&TOT>0,NA, ifelse(SP+SF==0,0,SP/(SP+SF)*TOT)/Duration/NB))
-set.sum$SF_EM <- with(set.sum,ifelse(SP+SF==0&TOT>0,NA, ifelse(SP+SF==0,0,SF/(SP+SF)*TOT)/Duration/NB))
-set.sum$julian <- julian(set.sum$DATE_RETRIEVED, origin = "1990-01-01")
-test2 <- ddply(set.sum,.(SITE), summarize, diff= diff(julian))
-set.sum <- subset(set.sum, !is.na(TOT)&Duration<45)
-set.sum$SITE_NUM <- as.numeric(set.sum$SITE)
-set.sum$M1 <- cos(2*pi*set.sum$month_ret/26)
-set.sum$M2 <- cos(2*pi*set.sum$month_ret/26)
-set.sum$yday <- yday(set.sum$DATE_RETRIEVED)
-set.sum$biweek  <- as.numeric(floor(set.sum$yday/14)+1)
-set.sum$biweek  <- ifelse(set.sum$biweek>26,26,set.sum$biweek)
-set.sum$biweek_year  <- set.sum$biweek+(set.sum$year_ret-1990)*26
 site_levels <- c("Anacapa[SB]","Fort Bragg[NorCal]","Gaviota[SB]","Ocean Beach[SD]","Ellwood[SB]","Stearns Wharf[SB]","Scripps[SD]")
 
-<<<<<<< HEAD
-set.monyr <- ddply(set.sum,.(biweek_year,SITE),summarise, mean_SP= mean( SP_EM),mean_SF= mean( SF_EM))
-=======
-set.monyr <- ddply(set.sum,.(biweek_year,SITE),summarise, mean_= mean(SP))
->>>>>>> 5e19371b5b5d5f342bfd7f3dfe7f19717d2a7cbd
+set.monyr <- set.sum %>%
+              group_by(biweek_year,SITE)%>% 
+                summarise(mean_SP= mean( SP_EM),mean_SF= mean( SF_EM)) %>%
+              mutate(site= factor(SITE,levels=levels(SITE),labels= site_levels))
 
 site <- 1:7
-data_mat <- expand.grid(site=site,biweek=1:26, YEAR= 1990:2015)
-data_mat$SITE <- factor(data_mat$site,labels= levels(set.sum$SITE))
-data_mat$biweek_year  <- data_mat$biweek+(data_mat$YEAR-1990)*26
-data_mat$MID <- 1:nrow(data_mat)
+data_mat2 <- expand.grid(site=site_levels,biweek=1:26, YEAR= 1990:2015) %>%
+              mutate(site=factor(site,levels= site_levels),
+                               biweek_year  = biweek+(YEAR-1990)*26,
+                               MID = 1:nrow(data_mat)) %>%
+            join(set.monyr)  
 
-set.sum2 <- join(data_mat,set.sum) 
-obs_SF <- matrix(set.sum2$mean, ncol= 7, byrow= T)
+obs_SF <- matrix(set.sum2$mean_SF, ncol= 7, byrow= T)
+obs_SP <- matrix(set.sum2$mean_SP, ncol= 7, byrow= T)
+
 
 
                                                                                    
