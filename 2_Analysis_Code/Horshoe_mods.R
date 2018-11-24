735c87f30042bdce83817de5437434db4fed41b9
dkokamoto <dokamoto@bio.fsu.edu>
2018-11-24 18:30:57 +0100

    Cleaned superfluous lines

--- 2_Analysis_Code/Horshoe_mods.R
+++ 2_Analysis_Code/Horshoe_mods.R
@@ -3,67 +3,17 @@
 ###  Author:  D.K. Okamoto                                 ###
 ##############################################################
 
-rm(list=ls())
 package.list<-c("abind","car","gdata","ggplot2","Hmisc","labeling","lubridate",
                 "mvtnorm","plyr","RColorBrewer","reshape2","scales","sp","rstan",
                 "dplyr","glmmTMB","parallel")
 
-setwd("Urchin_Settlement_Analysis")
 ### install packages if you don't have them 
 ### not run
 #lapply(package.list,install.packages)
 lapply(package.list,library,character.only=T)
 
 ### load urchin data ###
-settlement <- read.csv("1_Data/Invertebrate_Settlement_All_Years.csv",header=T)
-
-### format dates, etc.
-settlement <-  mutate(settlement, 
-                      DATE_DEPLOYED=parse_date_time(DATE_DEPLOYED, "%m/%d/%y"),
-                      DATE_RETRIEVED=parse_date_time(DATE_RETRIEVED, "%m/%d/%y"))
-
-settlement <- mutate(settlement,
-                     julian= as.numeric(julian(DATE_RETRIEVED)),
-                     ID = paste(DATE_DEPLOYED,DATE_RETRIEVED,SITE,sep= "_"),
-                     ID2 =paste(DATE_DEPLOYED,SITE,sep= "_"),
-                     ID3 =paste(DATE_RETRIEVED,SITE,sep= "_"),
-                     month_dep= month(DATE_DEPLOYED),
-                     month_ret = month(DATE_RETRIEVED),
-                     year_dep = year(DATE_DEPLOYED),
-                     year_ret = year(DATE_RETRIEVED),
-                     day_dep = day(DATE_DEPLOYED),
-                     day_ret = day(DATE_RETRIEVED))
-
-
-settlement <- settlement%>%
-  mutate(diff = as.numeric(Duration),
-         S_PURPURATUS = ifelse(S_PURPURATUS==-99999,NA,S_PURPURATUS),
-         M_FRANCISCANUS = ifelse(M_FRANCISCANUS==-99999,NA, M_FRANCISCANUS),
-         TOTAL_URCHINS = ifelse(TOTAL_URCHINS==-99999,NA, TOTAL_URCHINS))%>%
-  filter(!is.na(M_FRANCISCANUS)&
-           !is.na(S_PURPURATUS)&
-           !is.na(TOTAL_URCHINS)&
-           Duration>0) %>%
-  arrange(SITE,julian) %>%
-  drop.levels()
-
-
-
-### format data to estimate collection period means per brush per day
-set.sum <- settlement %>% group_by(SITE,DATE_RETRIEVED,DATE_DEPLOYED,month_ret,year_ret,Duration) %>%
-  mutate(SP= S_PURPURATUS,
-         SF= M_FRANCISCANUS,
-         TOT= TOTAL_URCHINS) %>%
-  dplyr::select(SITE,DATE_RETRIEVED,DATE_DEPLOYED,month_ret,year_ret,Duration,SP,SF,TOT)%>%
-  mutate(SP_EM= as.numeric(ifelse(SP+SF==0&TOT>0,NA, ifelse(SP+SF==0,0,SP/(SP+SF)*TOT)/Duration)),
-         SF_EM = as.numeric(ifelse(SP+SF==0&TOT>0,NA, ifelse(SP+SF==0,0,SF/(SP+SF)*TOT)/Duration)),
-         julian = julian(DATE_RETRIEVED, origin = "1990-01-01"),
-         SITE_NUM = as.numeric(SITE),
-         yday = yday(DATE_RETRIEVED),
-         biweek  = floor(yday/14)+1) %>%
-  mutate(biweek =ifelse(biweek>26,26,biweek),
-         biweek_year  = biweek+(year_ret-1990)*26,
-         NB=1)
+set.sum <- read.csv("1_Data/settlement_data_prepped.csv")
 
 D_site_star <-as.matrix(dist(unique(covariates$biweek_year),upper= T,diag= T))
 yearly <- seq(10,max(unique(covariates$biweek_year)),by = 26)
@@ -110,7 +60,6 @@ w <- rnorm(NYM,0,1)
 w2 <- rnorm(NT,0,1)
 plot(c(t(chol(Cstar))%*%w,t(chol(Cstar))%*%w),type= "l")
 plot(c(t(chol(Cstar2))%*%w2),type= "l")
-
 
 ### global covariates 
 covariates <- read.csv("1_Data/bayes_covariates.csv")[,-1]%>%
